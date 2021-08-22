a19228f2b52560debfaa76e7f8ec762afab98a82
Macbookair <macbookair@MacbookairdeMacBook-Air.local>
2021-08-22 12:15:18 +0800

    Added comments to increase readability

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+// require packages used in the project
 const express = require('express')
 const app = express()
 const port = 3000
-const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 
+// require express-handlebars
+const exphbs = require('express-handlebars')
+
+// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// setting static files
 app.use(express.static('public'))
 
+// routes setting
 app.get('/', (req, res) => {
     res.render('index', { restaurants: restaurantList.results })
 })
@@ -27,6 +34,7 @@ app.get('/search/', (req, res) => {
     }
 })
 
+// start and listen on the Express server
 app.listen(port, () => {
     console.log(`Express is listening on http://localhost:${port}`)
 })
