7e532ae20259b705b50b4fb97845d0452ccf946c
chaco <iloveb9999@gmail.com>
2021-09-13 11:40:12 +0800

    refactor: modify mongoose config

--- Models/seeds/restaurantSeeder.js
+++ Models/seeds/restaurantSeeder.js
@@ -1,11 +1,6 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const Restaurant = require('../restaurant')
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
 
-db.on('error', () => {
-    console.log('mongodb error!')
-})
 db.once('open', () => {
     console.log('mongodb connected!')
     Restaurant.create({
--- app.js
+++ app.js
@@ -6,21 +6,13 @@ const Restaurant = require('./models/restaurant')
 
 // require express-handlebars
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
-const routes = require('./routes')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const routes = require('./routes')
+require('./config/mongoose')
 
-const db = mongoose.connection
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-db.once('open', () => {
-        console.log('mongodb connected!')
-    })
-    // setting template engine
+// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -28,7 +20,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 app.use(express.static('public'))
-
 
 // start and listen on the Express server
 app.listen(port, () => {
